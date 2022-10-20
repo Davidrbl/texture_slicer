e0ab9b3f64857dfc930d2cd6cc7d7f7369307673
Finlay Davidson <finlay.davidson@coderclass.nl>
2022-10-20 16:08:51 +0200

    Fix incorrect pathnames

--- build_x64.bat
+++ build_x64.bat
@@ -3,10 +3,10 @@ set flags=-Wall
 set INCLUDE_FOLDER=./include
 @echo on
 
-gcc -c ./src/main.c %flags% -I %INCLUDE_FOLDER% -o main.o
-gcc -c ./src/shader.c %flags% -I %INCLUDE_FOLDER% -o shader.o
-gcc -c ./src/gl.c %flags% -I %INCLUDE_FOLDER% -o gl.o
-gcc -c ./src/bmp_imp.c %flags% -I %INCLUDE_FOLDER% -o bmp_imp.o
-gcc -c ./src/texture_slicing.c %flags% -I %INCLUDE_FOLDER% -o texture_slicing.o
+gcc -c ./src/main.c %flags% -I %INCLUDE_FOLDER% -o obj/main.o
+gcc -c ./src/shader.c %flags% -I %INCLUDE_FOLDER% -o obj/shader.o
+gcc -c ./src/gl.c %flags% -I %INCLUDE_FOLDER% -o obj/gl.o
+gcc -c ./src/bmp_imp.c %flags% -I %INCLUDE_FOLDER% -o obj/bmp_imp.o
+gcc -c ./src/texture_slicing.c %flags% -I %INCLUDE_FOLDER% -o obj/texture_slicing.o
 
-gcc main.o shader.o gl.o bmp_imp.o texture_slicing.o glfw3.dll -o main.exe
+gcc obj/main.o obj/shader.o obj/gl.o obj/bmp_imp.o obj/texture_slicing.o -lglfw3 -o main.exe
